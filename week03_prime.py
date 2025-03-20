877b3859ea13cef451123428d87bd44886554294
JEMIN PARK <202130138@naver.com>
2025-03-20 11:55:04 +0900

    v0.3 prime number

--- week03_prime.py
+++ week03_prime.py
@@ -2,11 +2,19 @@ number = int(input())
 count = 0
 
 for i in range(1, number + 1):
-    if number % i == 0: 
+    if number % i == 0:
         count = count + 1
     print(i, end=" ")
-    
-if count == 2:
-    print(f"{number}는(은) 소수입니다")
+if number >= 2:
+    for i in range(2, number):
+        if number % i == 0:
+            count = count + 1
+        print(i, end=" ")
 else:
-    print(f"{number}는(은) 소수가 아닙니다")
+    count = 1
+
+if count == 2:
+    if count == 0:
+        print(f"{number}는(은) 소수입니다.")
+    else:
+        print(f"{number}는(은) 소수가 아닙니다!")
