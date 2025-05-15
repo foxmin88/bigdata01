7f2ce59165cdc72b8d5eb0b5af244e2cc8afb20c
JEMIN PARK <202130138@naver.com>
2025-05-15 12:18:32 +0900

    v3.4 kiosk

--- kiosk.py
+++ kiosk.py
@@ -1,4 +1,5 @@
 import datetime
+import sqlite3
 
 drinks = ["아이스 아메리카노", "카페 라떼", "수박 주스", "딸기 주스"]
 prices = [2000, 2500, 4000, 4200]
@@ -39,18 +40,28 @@ def print_ticket_number() -> None:
     주문 번호표 출력 함수
     :return: None
     """
-    try:
-        with open("ticket.txt", "r") as fp:
-            number = int(fp.read())
-    except FileNotFoundError:
-        number = 0
+    conn = sqlite3.connect("cafe.db")
+    cur = conn.cursor()
 
-    number = number + 1
+    cur.execute('''
+        create table if not exists ticket (
+        id integer primary key autoincrement,
+        number integer not null
+        )
+    ''')
 
-    with open("ticket.txt", "w") as fp:
-        fp.write(str(number))
+    cur.execute('select number from ticket order by number desc limit 1')
+    result = cur.fetchone()
 
-    # return number
+    if result is None:
+        number = 1
+        cur.execute('insert into ticket (number) values (?)', (number,))
+    else:
+        number = result[0] + 1
+        cur.execute('insert into ticket (number) values (?)', (number,))
+
+    conn.commit()
+
     print(f'번호표 : {number}')
 
 def order_process(idx: int) -> None:
